0a7f74cb67e74fbe6a3591dbba4c36a9df74ead0
Timur Cravtov <timurcravtov04@gmail.com>
2026-03-16 21:40:43 +0200

    feat(printer): adjust image printing options and handling

--- internal/printer/image.go
+++ internal/printer/image.go
@@ -1,8 +1,41 @@
 package printer
 
-import "go2web/internal/connect"
+import (
+    "image"
+    "bytes"
+    "go2web/internal/connect"
+    "go2web/internal/printer/utils"
+)
 
 func ImagePrinter(urlPath string, response *connect.HttpResponse) (string, error) {
-	return "[Image content not displayed]", nil
+    imageBytes := response.Body
+
+    config, _, err := image.DecodeConfig(bytes.NewReader(imageBytes))
+    if err != nil {
+        return "", err
+    }
+
+    width, height := calculateBestDimensions(config.Width, config.Height)
+
+    asciiArt, err := utils.ImageToAscii(imageBytes, width, height)
+    if err != nil {
+        return "", err
+    }
+    return asciiArt, nil
 }
 
+func calculateBestDimensions(originalWidth, originalHeight int) (int, int) {
+    maxWidth := 60
+    
+    ratio := float64(originalWidth) / float64(originalHeight)
+        
+    newWidth := maxWidth
+    newHeight := int(float64(newWidth) / (ratio * 2.0))
+
+    if newHeight > 40 {
+        newHeight = 40
+        newWidth = int(float64(newHeight) * ratio * 2.0)
+    }
+
+    return newWidth, newHeight
+}
